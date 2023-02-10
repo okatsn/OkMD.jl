ec6abaf27346246e583b2865b5042f41af086e99
Documenter.jl <documenter@juliadocs.github.io>
2023-02-10 08:55:40 +0000

    build based on 00a4cf7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OkMD","category":"page"},{"location":"#OkMD","page":"Home","title":"OkMD","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OkMD.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OkMD]","category":"page"}]
+}
